bb35e3f6407fd9bffe47fbc17d471ae3b80b7c35
maya elabed <56444556+mayaelabed@users.noreply.github.com>
2023-11-09 11:38:48 +0100

    Add files via upload

---
+++ ThymDemo/src/test/java/com/example/ThymDemo/ThymDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThymDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
